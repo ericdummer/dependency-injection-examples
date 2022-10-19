4d1735e39f73b5bbd6409f14853aaf4994649de6
edummer <edummer@entrata.com>
2022-10-19 10:10:03 -0600

    a branch where the no tests are written yet

--- tests/MySite/ExampleTest.php
+++ tests/MySite/ExampleTest.php
@@ -9,50 +9,11 @@ use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
-
-  public static function setUpBeforeClass(): void {
-    ServiceContainer::getContainer()->setService('CsvReader', new CsvReader());
-  }
-
-  public function testNonsensicalAllConstructor(): void
-  {
-    /** @var Dictionary|MockObject $dictionaryMock */
-    $dictionaryMock = $this->createMock(Dictionary::class);
-    $dictionaryMock->method('getRandomWord')
-      ->will(
-        $this->onConsecutiveCalls("one", "two", "three")
-      );
-
-    /** @var WordProcessor|MockObject $wordProcessorMock */
-    $wordProcessorMock = $this->createMock(WordProcessor::class);
-
-    $sut = new Example($dictionaryMock, $wordProcessorMock);
-    $results = $sut->getNonsensicalSentence(3);
-    $expected = "one two three";
-    $this->assertEquals($expected, $results);
-
+  public function testNonsensicalAllConstructor(): void {
+    $this->markTestSkipped("IMPLEMENT ME!");
   }
 
   public function testScoreAllConstructors(): void {
-
-    /** @var Dictionary|MockObject $dictionaryMock */
-    $dictionaryMock = $this->createMock(Dictionary::class);
-    $dictionaryMock->method('getRandomWord')
-      ->will(
-        $this->onConsecutiveCalls("one", "two", "three")
-      );
-
-    /** @var WordProcessor|MockObject $wordProcessorMock */
-    $wordProcessorMock = $this->createMock(WordProcessor::class);
-    $wordProcessorMock->method('getFrequencyOfWord')
-      ->will(
-        $this->onConsecutiveCalls(1, 2, 3)
-      );
-
-    $sut = new Example($dictionaryMock, $wordProcessorMock);
-    $results = $sut->getSentenceScore("one two three");
-    $expected = 6;
-    $this->assertEquals($expected, $results);
-
+    $this->markTestSkipped("IMPLEMENT ME!");
   }
 }
--- tests/MySite/Library/DictionaryTest.php
+++ tests/MySite/Library/DictionaryTest.php
@@ -8,24 +8,13 @@ use PHPUnit\Framework\TestCase;
 class DictionaryTest extends TestCase
 {
 
-    public function testGetDictionary()
-    {
-      /** @var CsvReader|MockObject $csvReaderMock */
-      $csvReaderMock = $this->createMock(CsvReader::class);
-      $csvReaderMock->expects($this->once())->method('loadFile');
-      ServiceContainer::getContainer()->setService('CsvReader', $csvReaderMock);
-
-      $sut = new Dictionary();
-      $sut->getDictionary();
-    }
+  public function testGetDictionary()
+  {
+    $this->markTestSkipped("IMPLEMENT ME!");
+  }
 
   public function testGetDictionaryFromFile()
   {
-    /** @var CsvReader|MockObject $csvReaderMock */
-    $csvReaderMock = $this->createMock(CsvReader::class);
-    $csvReaderMock->expects($this->once())->method('loadFile');
-
-    $sut = new Dictionary();
-    $sut->getDictionaryFromFile(Dictionary::DEFAULT_FILE, $csvReaderMock);
+    $this->markTestSkipped("IMPLEMENT ME!");
   }
 }
